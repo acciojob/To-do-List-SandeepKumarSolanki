f861b48253011863e3476eb5f8ddb9b9f4b821a7
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-14 07:37:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
+let InputField = document.getElementById('newTodoInput')
+let btn = document.getElementById('addTodoBtn');
+let todoList = document.getElementById('todoList');
 
+btn.addEventListener('click' , (e)=>{
+	e.preventDefault();
+	let InputFieldValue = InputField.value;
+	let li = document.createElement('li');
+	li.innerText = `${InputFieldValue}`
+	todoList.append(li);
+	InputField.value = '';
+})
